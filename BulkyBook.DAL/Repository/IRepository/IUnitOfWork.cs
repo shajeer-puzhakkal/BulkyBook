ae1fca1e8657b36054f9d078856c91ac3b708734
Shajeer <mshajeer@ucssolutions.com>
2020-11-09 17:59:57 +0400

    Impelemented UoW for Dependency Injection

---
+++ BulkyBook.DAL/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DAL.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get;}
+        ISP_Call SP_Call { get;}
+
+    }
+}
---
+++ BulkyBook.DAL/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using BulkyBook.DAL.Repository.IRepository;
+using BulkyBook.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DAL.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+    }
+}
